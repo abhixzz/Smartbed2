ede5d0dcc208d7a5dbab921904008d7b4222f96e
ATL VKD <atlghssvkd@gmail.com>
2025-12-24 06:15:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "medivue2-29630051-b769c",
+  "appId": "1:640218025032:web:519a225758575e1168dc7d",
+  "apiKey": "AIzaSyArK0D0vSZiUZdEwPSbSunKJ0uqDjEa6TM",
+  "authDomain": "medivue2-29630051-b769c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "640218025032"
+};
